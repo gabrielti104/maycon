bebd7ff48c867af63deebe32d4902e5842ad11b1
Gabriel <106531227+gabrielti104@users.noreply.github.com>
2023-10-02 09:37:29 -0300

    Add files via upload

---
+++ Lojacomputadores/src/main/java/com/mycompany/lojacomputadores/Lojacomputadores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lojacomputadores;
+
+/**
+ *
+ * @author maycon.mssouza
+ */
+public class Lojacomputadores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
