06a2a93142b52a18bb1d2737b7c243d707230df8
Gabriel <106531227+gabrielti104@users.noreply.github.com>
2023-10-02 09:38:01 -0300

    Add files via upload

---
+++ exemplojanelas/src/main/java/com/mycompany/exemplojanelas/Exemplojanelas.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.exemplojanelas;
+
+ class Exemplojanelas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ exemplojanelas/src/main/java/com/mycompany/exemplojanelas/NewMain.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.exemplojanelas;
+
+import javax.swing.UIManager;
+
+
+public class NewMain {
+
+  
+    public static void main(String[] args) {
+        
+        UIManager.setLookAndFeel(UIManager.getInstalledLookAndFeels());
+    }
+    
+}
